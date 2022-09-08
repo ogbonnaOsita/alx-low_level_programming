0d88cc46093b423ace0557ca429b7f0f978493f8
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-08 11:49:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-
-
 /**
  *
  *  * main - prints the phrase "Programming is like building a multilingual puzzle
  *
- *   * Return: 0 if exited properly, non-zero otherwise
+ *  * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
 
@@ -14,7 +11,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle");
 
 			return (0);
 
