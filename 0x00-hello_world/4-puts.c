86592f8485ec16a8ce527b6de646defce27797d6
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-08 12:03:15 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  *
- *  main - prints the phrase "Programming is like building a multilingual puzzle"
+ *  main - Entry point
  *
- *  Return: 0 if exited properly, non-zero otherwise
+ *  Return: Always 0 (Success)
  *
  */
 
