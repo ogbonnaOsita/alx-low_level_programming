d72bd8786a9150d4ac75db5efd3b71b1025b55ed
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-08 11:54:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,8 +11,7 @@ int main(void)
 
 {
 
-		puts("Programming is like building a multilingual puzzle");
-
-			return (0);
+printf("Programming is like building a multilingual puzzle\n");
+return (0);
 
 }
