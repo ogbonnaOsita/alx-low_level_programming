794de529db3931c41fef60d861cd95b0c4bbf7d4
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:36:40 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * Return: Always 1 (Success)
  */
 
-int _isalpha(letter)
+int _isalpha(int letter)
 {
-	return ((letter >= 'a'  && letter <= 'z' || (letter >= 'A' && letter <= 'Z'));
+	return ((letter >= 'a' && letter <= 'z' || (letter >= 'A' && letter <= 'Z'));
 			}
