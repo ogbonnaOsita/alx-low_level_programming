2bbf9d9082b905044b4552bc2b71a217da61b245
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:33:22 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isalpha - check if the character is alphabetic
+ * @letter: The character to be checked
+ * Return: Always 1 (Success)
+ */
+
+int _isalpha(letter)
+{
+	return ((letter >= 'a'  && letter <= 'z' || (letter >= 'A' && letter <= 'Z'));
+			}
