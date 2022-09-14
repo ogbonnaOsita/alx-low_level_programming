2c66c640105facbc608df780fe8ee6eee7a11bdf
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:10:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase 
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+
+	-putchar('\n');
+}
