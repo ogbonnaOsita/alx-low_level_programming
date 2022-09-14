281b88dce2115ef5fd57ce30eb053ef7f7ece30e
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:45:34 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,11 @@ int print_sign(int number)
 	{
 		_putchar('+');
 		return(1);
-	}else if (n == 0)
+	}else if (number == 0)
 	{
 		_putchar(48);
 		return (0);
-	}else if (n < 0)
+	}else if (number < 0)
 	{
 		_putchar('-');
 	}
