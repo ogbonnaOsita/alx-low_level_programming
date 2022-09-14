190e57ca2b7fe8c9565e52d26411616c44191609
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:53:23 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value for an integer
+ * @n: the number to check
+ * Return: the required value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
