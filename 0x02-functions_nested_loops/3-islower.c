aacb3b5a9b75b352b60d01f62949e6bf2c26c5fc
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:26:00 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - check for lowercase character
+ * Return: Always 1 (Success)
+ */
+
+int _islower(int letter)
+{
+	return (letter >= 'a'  && letter <= 'z');
+}
