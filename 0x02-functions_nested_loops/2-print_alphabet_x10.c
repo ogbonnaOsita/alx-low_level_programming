44a759c1735334b5abd15ce57b30bfbbe8d1a812
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-13 18:21:30 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	i = 0;
+
+	while (i <10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
