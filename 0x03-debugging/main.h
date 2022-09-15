b403cf74b5eae72ca9cae202e2fd9edaaacb935c
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-15 12:10:22 -0700

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,6 @@
 /**
  * positive_or_negative - checks 
  * if a number is positive or negative
- * Return: Always 0 (Success)
  */
 void positive_or_negative(int i)
 {
@@ -18,7 +17,6 @@ void positive_or_negative(int i)
 		printf("%d  is zero\n", i);
 	else if (n < 0)
 		printf("%d is negative\n", i);
-	return (0);
 }
 
 #endif /* MAIN_H */
