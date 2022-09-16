930e1c2b5e34d0efbd921662f31f83f8ff0d1073
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-15 18:59:24 -0700

    update

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * check_sign - function that prints if integer is positive or negative
+ * main - function that prints if integer is positive or negative
  * Return: 0
  */
 
-int check_sign(void)
+int main(void)
 {
 	int i;
 
