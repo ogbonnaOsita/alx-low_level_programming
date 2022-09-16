fcfb5fbdd867a5112104fd21e16a77aec5f9499b
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-15 20:14:45 -0700

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - draws a straight line in the terminal
- * @n: number of times to print the character _
+ * @c: number of times to print the character _
  */
 
 void print_line(int c)
