c9b44f55d9114a4b782f8c5d7c9bbadc6d001c1b
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-15 20:05:39 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times to print the character _
+ */
+
+void print_line(int c)
+{
+	if (c <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= c; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
+
