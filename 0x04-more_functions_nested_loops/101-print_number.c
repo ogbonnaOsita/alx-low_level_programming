aa23393a16ab325bcf929117da1d077863a14918
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-16 21:35:32 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number -  prints an integer
+ * @n: the integer to print
+ */
+
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	} else
+	{
+		x = n;
+	}
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+	_putchar((x % 10) + '0');
+}
