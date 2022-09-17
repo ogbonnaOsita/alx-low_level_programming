27a5c7d6dcf579cc9fde3f17588c919dd9ee2017
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-16 19:50:53 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * print_diagonal - prints a diagonal line
- * @n: number of times to print the character  \
+ * @n: number of times to print the character
  */
-
-void pirnt_diagonal(int n)
+void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
