17d4613311fb026f9433bd317ed316534cc0de3c
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-19 11:57:02 -0700

    update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - used to reverse a sring
+ * @s: the string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	char str1;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len -1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		str1 = s[i];
+		s[i] = s[len1];
+		s[len1--] = str1;
+	}
+}
