a42d9a6bc903133a2b0610ba4735ee791d879a48
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-19 12:51:17 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,28 +17,20 @@ int _atoi(char *s)
 	f = 0;
 
 	while (s[d] != '\0')
-	{
 		d++;
-	}
 	while (a < d && e == 0)
 	{
 		if (s[a] == '-')
-		{
 			++b;
-		}
 		if (s[a] >= '0' && s[a] <= '9')
 		{
 			f = s[a] - '0';
 			if (b % 2)
-			{
 				f = -f;
-			}
 			c = c * 10 + f;
 			e = 1;
 			if (s[a + 1] < '0' || s[a + 1] > '9')
-			{
 				break;
-			}
 			e = 0;
 		}
 		a++;
