8f79db8b7b6dc8bd105f188b7b176c4fd4822e4a
Ogbonna Osita <osita.ogbonna.21@gmail.com>
2022-09-21 12:34:03 -0700

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - it compares 2 strings
+ * @sting1: the first string
+ * @string2: the second string
+ * Return: 0 if string1 is less than string2
+ * 0 if they are equal
+ */
+
+int _strcmp(char *string1, char *string2)
+{
+	while (*string1 == *string2)
+	{
+		if (*string1 == '\0')
+		{
+			return (0);
+		}
+		string1++;
+		string2++;
+	}
+	return (*string1 - *string2);
+}
